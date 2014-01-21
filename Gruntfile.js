cf4a1b0521cee2c99596c26769a1932a27cb3922
Justin H. Johnson <justin.hills.johnson@gmail.com>
2014-01-21 15:34:11 -0700

    Docs folder for complexity

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,8 +32,8 @@ module.exports = function(grunt) {
         'src': [config.javascripts + '/app/**/*.js'],
         'options': {
           'breakOnErrors': true,
-          'jsLintXML': 'report.xml', // create XML JSLint-like report
-          'checkstyleXML': 'checkstyle.xml', // create checkstyle report
+          'jsLintXML': config.docsbin + '/report.xml', // create XML JSLint-like report
+          'checkstyleXML': config.docsbin + '/checkstyle.xml', // create checkstyle report
           'errorsOnly': false, // show only maintainability errors
           'cyclomatic': [3, 7, 12], // or optionally a single value, like 3
           'halstead': [8, 13, 20], // or optionally a single value, like 8
